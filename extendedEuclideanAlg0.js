d9cb932b0018feeff08a07d83698bb8bb883965b
MahendrSH <maahi2001sh@gmail.com>
2023-06-06 22:17:02 +0530

    create : qestion 2 : Extended Euclidean Algo

--- extendedEuclideanAlg0.js
+++ extendedEuclideanAlg0.js
@@ -17,14 +17,10 @@ function gcdExtended(a, b,
     return gcd;
 }
 
-// Driver Code
 let x = 0;
 let y = 0;
 let a = 35;
 let b = 15;
 let g = gcdExtended(a, b, x, y);
-console.log("gcd(" + a);
-console.log(", " + b + ")");
-console.log(" = " + g);
-
+console.log( ` gcd ( ${a} , ${b} ) = ${g} `);
 
